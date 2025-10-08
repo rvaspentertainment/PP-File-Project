4cec4559d15a70bb708317f5baea2db2cd61690f
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-10-08 20:30:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'AshutoshGoswami24,BotzPW').split(',')
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003013971591"))
     PORT = int(os.environ.get("PORT", ""))
     
     # 4GB upload support (requires premium account session)
